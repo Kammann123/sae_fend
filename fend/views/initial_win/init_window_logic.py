30c212a752f2a59d0491b6417d52bf4e543b7bd2
Nico <rtrozzo@itba.edu.ar>
2019-07-22 14:17:47 -0300

    initial window upgrades

--- fend/views/initial_win/init_window_logic.py
+++ fend/views/initial_win/init_window_logic.py
@@ -6,22 +6,25 @@ class InitWindow(QtWidgets.QDialog, Ui_InitWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QDialog.__init__(self, *args, **kwargs)
         self.setupUi(self)
-        self.timer = threading.Timer(5.0, self.stop_showing)
-        self.movingTimer = threading.Timer(0.1, self.update_animation)
+        self.timer = threading.Timer(30.0, self._stop_showing)
+        self.movingTimer = threading.Timer(0.025, self.update_animation)
+        self.start_showing()
 
     def start_showing(self):
         self.show()
         self.timer.start()
+        self.movingTimer.start()
 
-    def stop_showing(self):
+    def _stop_showing(self):
         self.hide()
 
-    #Red car animation
+    # Red car animation
     def update_animation(self):
-        x1, y1, w, h = self.redCar.geometry().getRect()
-        xf = self.geometry().getRect()
-        if x1 < xf:
-            self.redCar.setGeometry(x1 + 10, y1, w, h)
+        x_car, y_car, w_car, h_car = self.redCar.geometry().getRect()
+        x_win, y_win, w_win, h_win = self.geometry().getRect()
+        if x_car < w_win:
+            self.redCar.setGeometry(x_car + 5, y_car, w_car, h_car)
         else:
-            self.redCar.setGeometry(-70, y1, w, h)
-
+            self.redCar.setGeometry(-70, y_car, w_car, h_car)
+        self.redCar.show()
+        self.movingTimer.run()
