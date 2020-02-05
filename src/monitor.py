720828d45668ffc6ac1c291d55400c23f570d2bc
Lucas Kammann <lucaskammann@hotmail.com>
2020-02-05 19:42:53 -0300

    The Monitor has now the Panel and the Slider

--- src/monitor.py
+++ src/monitor.py
@@ -1,8 +1,11 @@
 # PyQt5 modules
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import pyqtSlot
 
 # Widgets import
 from src.ui.monitor import Ui_Monitor
+from src.slider import ChartSlider
+from src.panel import Panel
 
 
 class Monitor(QWidget, Ui_Monitor):
@@ -16,3 +19,22 @@ class Monitor(QWidget, Ui_Monitor):
 
         # Setting the reference to the router parent
         self.router = router
+
+        # Adding widgets to the data stacked widget
+        self.panel = Panel()
+        self.slider = ChartSlider()
+        self.data_widget.addWidget(self.panel)
+        self.data_widget.addWidget(self.slider)
+        self.data_widget.setCurrentWidget(self.panel)
+
+        # Connecting signals and slots
+        self.panel_button.clicked.connect(self.go_panel)
+        self.graphic_button.clicked.connect(self.go_slider)
+
+    @pyqtSlot(name='goPanel')
+    def go_panel(self):
+        self.data_widget.setCurrentWidget(self.panel)
+
+    @pyqtSlot(name='goSlider')
+    def go_slider(self):
+        self.data_widget.setCurrentWidget(self.slider)
